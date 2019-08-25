8c0aa151fd3139cdcc9364db471f4fa0ac65cdf2
Michal Borowski <unpolacoo@gmail.com>
2019-08-25 09:53:33 +0200

    new UI - adding crayons

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 window.addEventListener("load", () => {
-    const canvas = document.getElementById("canvas");
+    var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
 
-    canvas.height = 400; //window.innerHeight;
-    canvas.width = 600; //window.innerWidth;
+    canvas.height = 400;
+    canvas.width = 600;
 
     var drawing = false;
+    var erazerStyle = false;
 
     function startDrawing() {
         drawing = true;
@@ -16,90 +17,119 @@ window.addEventListener("load", () => {
         ctx.beginPath();
     };
 
+    //active crayon add class
+
+
+    var crayonlist = document.getElementById("crayonlist");
+
+    crayonlist.addEventListener("click", function (e) {
+
+        let li = e.target.closest('li'); 
+        if (!li) return; 
+        if (!crayonlist.contains(li)) return;
+
+        var qqq = document.querySelectorAll(".crayon");
+
+        for(i=0; i < qqq.length; i++) {
+            console.log(qqq[i])
+           qqq[i].classList.remove("crayon_active");
+        };
+        
+        li.classList.add("crayon_active");
+
+    });
+
+    
+    
+    
+
+    //    erazer
+    var erazer = document.querySelector("#erazer");
+    erazer.addEventListener("click", erazeTool);
+
+    function erazeTool() {
+        erazerStyle = true;
+        ctx.strokeStyle = "#f0efef";
+    };
+
+    // pencil
+    var pencil = document.querySelector("#pencil");
+    pencil.addEventListener("click", pencilTool);
+
+    function pencilTool() {
+        pencilStyle = true;
+        ctx.strokeStyle = "#ff0000";
+    };
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////
     //set color
     var colorLine = document.querySelector("#choosecolor");
     colorLine.addEventListener("change", setColor);
 
-    function setColor() {
-        // czemu w console.log nie wywala mi odpowiedniej wartosci?
-        console.log(colorLine);
-        ctx.strokeStyle = colorLine.value;
-    };
+//    function setColor() {
+//        if (erazerStyle == true) {
+//            ctx.strokeStyle = "#fff";
+//        } else if (pencilStyle == true) {
+//            ctx.strokeStyle = colorLine.value;
+//        } else if (crayonStyle == true) {
+//            ctx.strokeStyle = "#00ff00"
+//            ctx.lineWidth = 5;
+//        }
+//    };
 
     //set linewidth
     var lineWidth = document.querySelector("#lineWight");
     lineWidth.addEventListener("change", setLineWight);
 
     function setLineWight() {
-        console.log(lineWight.value);
         ctx.lineWidth = lineWidth.value;
     }
-
-
-    //continous line
-    //var oneLine = document.querySelector("oneLine");
-    //function drawOneLine() {
-    //    
-    //}
-
-
 
     // load image to colorize
     var loadImage = document.querySelector("#loadImage");
     loadImage.addEventListener("click", newImage)
 
     function newImage() {
-        base_image = new Image();
-        //dodac randomowy obraz
+        var base_image = new Image();
         var randomImage = Math.floor(Math.random() * 3) + 1;
         base_image.src = 'img/img' + randomImage + '.png';
         base_image.onload = function () {
             ctx.clearRect(0, 0, 600, 400);
             ctx.drawImage(base_image, 0, 0, 600, 400);
-
-
-            //      document.querySelector('.btn-roll').addEventListener('click', function() {
-            //    if(gamePlaying) {
-            //        // 1. Random number
-            //        var dice1 = Math.floor(Math.random() * 6) + 1;
-            //        var dice2 = Math.floor(Math.random() * 6) + 1;
-            //
-            //        //2. Display the result
-            //        document.getElementById('dice-1').style.display = 'block';
-            //        document.getElementById('dice-2').style.display = 'block';
-            //        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
-            //        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
         }
     }
-    //erazer
 
     //set linetype
 
-    //set cursor
+    //save image
+    //var canvas = document.getElementById("myCanvas");
+    window.open(canvas.toDataURL("image/png"));
 
     //clean canvas   
+    var clean = document.querySelector("#clean");
+    clean.addEventListener("click", resetAll);
 
-    //continous line
-    //var oneLine = document.querySelector("oneLine");
-    //function drawOneLine() {
-    //    
-    //}
+    function resetAll() {
+        ctx.clearRect(0, 0, 600, 400);
+    };
+
 
 
     function draw(e) {
         if (!drawing) return;
         ctx.lineCap = "round";
-        ctx.lineTo(e.clientX, e.clientY);
+        ctx.lineTo(e.clientX - 15, e.clientY - 15);
         ctx.stroke();
         ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY);
+        ctx.moveTo(e.clientX - 15, e.clientY - 15);
         //ctx.strokeStyle = "rgba (0,0,20,1)";
-        ctx.strokeStyle = colorLine.value;
+        //ctx.strokeStyle = colorLine.value;
         ctx.lineWidth = lineWidth.value;
-        //draw image after drawing
-        ctx.drawImage(base_image, 0, 0, 600, 400);
+        //ctx.drawImage(base_image, 0, 0, 600, 400);
 
-    };
+    }
+
 
     canvas.addEventListener("mousedown", startDrawing);
     canvas.addEventListener("mouseup", stopDrawing);
